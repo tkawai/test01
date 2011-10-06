4048a387095a7d8052583105f5692bc0eb145c60
tkawai <kawai.t@tabot.jp>
2011-10-06 12:20:59 +0900

    commit

---
+++ getTest/src/test/pk/GitTestActivity.java
@@ -0,0 +1,13 @@
+package test.pk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GitTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
